48da8e423934ff25b81af46c29548adef1e90678
Jakub Olszewski <jakub@olszewski.pl>
2019-03-23 00:13:56 +0100

    Hello World

---
+++ src/main/java/pl/sdacademy/java16poz/App.java
@@ -0,0 +1,23 @@
+package pl.sdacademy.java16poz;
+//generate test
+//alt + enter
+
+/**
+ * App
+ *
+ * @author: Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @date: 14.10.2018 10:00
+ **/
+public class App {
+    //code format
+    //ctrl+alt+L
+
+    // alt + insert + enter
+    // generate code
+
+    //psvm + tab
+    public static void main(String[] args) {
+        //sout + tab
+        System.out.println("Hello World!");
+    }
+}
