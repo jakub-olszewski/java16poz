a4b921ec2fa4787ab2c6de4a1abed4256e227bf0
Jakub Olszewski <jakub@olszewski.pl>
2019-03-24 13:19:11 +0100

    Zadanie: Interfejsy

---
+++ src/main/java/pl/sdacademy/java16poz/interfejsy/McDonaldMain.java
@@ -0,0 +1,20 @@
+package pl.sdacademy.java16poz.interfejsy;
+
+/**
+ * McDonaldMain
+ *
+ * @author: Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @date: 24.03.2019 13:17
+ **/
+public class McDonaldMain {
+
+    public static void main(String[] args) {
+
+        McDonald dworzecMcDonald = new DworzecMcDonaldImpl();
+
+        // utworzenie implementacji z CH Posnania
+
+        // wywołanie dostępnych metod
+
+    }
+}
