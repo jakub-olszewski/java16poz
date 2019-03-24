5d92c8af3d542c2e3da8329909769863568f05ba
Jakub Olszewski <jakub@olszewski.pl>
2019-03-24 11:55:22 +0100

    Zadanie: Klasa trojkat + metody + pola

---
+++ src/main/java/pl/sdacademy/java16poz/klasy/TrojkatMain.java
@@ -0,0 +1,16 @@
+package pl.sdacademy.java16poz.klasy;
+
+/**
+ * TrojkatMain
+ *
+ * @author: Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @date: 24.03.2019 11:51
+ **/
+public class TrojkatMain {
+
+    // utworzenie trojkata
+    // obliczenie pola
+    // obliczenie obwodu
+
+    // wypisanie inforacji
+}
---
+++ src/main/java/pl/sdacademy/java16poz/obiekty/Trojkat.java
@@ -0,0 +1,21 @@
+package pl.sdacademy.java16poz.obiekty;
+
+/**
+ * Trojkat
+ *
+ * @author: Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @date: 24.03.2019 11:49
+ **/
+public class Trojkat {
+    // zalozenia trojkat prostokatny - nie sprawdzamy warunku trojkata
+
+    // odpowiednie pola
+
+    // konstruktor
+
+    // metody:
+    // * oblicz pole,  * - mnożenie   + - dodawanie
+    // * oblicz obwod
+    // * wypiszInfo() - informacje o trojkacie wszystkich
+
+}
