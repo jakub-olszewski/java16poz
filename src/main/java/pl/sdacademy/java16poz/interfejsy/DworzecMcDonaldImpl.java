0939847ffdd787586224b4e5f006c6d49fece6ab
Jakub Olszewski <jakub@olszewski.pl>
2019-03-24 12:54:26 +0100

    Teoria: Interfejsy

---
+++ src/main/java/pl/sdacademy/java16poz/interfejsy/DworzecMcDonaldImpl.java
@@ -0,0 +1,10 @@
+package pl.sdacademy.java16poz.interfejsy;
+
+/**
+ * DworzecMcDonaldImpl
+ *
+ * @author: Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @date: 24.03.2019 12:52
+ **/
+public class DworzecMcDonaldImpl {
+}
