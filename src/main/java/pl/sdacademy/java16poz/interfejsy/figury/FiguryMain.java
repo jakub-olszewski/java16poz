594972069c1e922dae31cc29231cf78ac56a6e07
Jakub Olszewski <jakub@olszewski.pl>
2019-03-24 13:51:30 +0100

    Zadanie: Interfejsy

---
+++ src/main/java/pl/sdacademy/java16poz/interfejsy/figury/FiguryMain.java
@@ -0,0 +1,35 @@
+package pl.sdacademy.java16poz.interfejsy.figury;
+
+/**
+ * FiguryMain
+ *
+ * @author: Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @date: 24.03.2019 13:44
+ **/
+public class FiguryMain {
+
+    public static void main(String[] args) {
+
+        /** interfejs: Figura
+         *  * metoda: float obliczPole()
+         *  * metoda: float obliczObwod()
+         *  * metoda: void wypiszDane() - pretty print, przyjazne dla użytkownika
+         *
+         *  Klasy: ( implementować interfejs Figura )
+         *  * TrojkatFiguraImpl
+         *  * KwadratFiguraImpl
+         *  * KoloFiguraImpl
+         *  * ProstokatFiguraImpl
+         *
+         *  Klasy zawieraja odpowiednie pola - wymagane do metod
+         *
+         *  Main:
+         *   Tworzenie wszystkich figur. Wypisanie pól i obwodów
+         *
+         *   Dodatkowowo: suma pól i obwodów
+         *
+         *
+         */
+
+    }
+}
