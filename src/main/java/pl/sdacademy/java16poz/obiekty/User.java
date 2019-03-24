8861c11737d69746dc8aa2cea9a3b08926847c52
Jakub Olszewski <jakub@olszewski.pl>
2019-03-24 15:40:59 +0100

    Zadanie: toString()

---
+++ src/main/java/pl/sdacademy/java16poz/obiekty/User.java
@@ -0,0 +1,10 @@
+package pl.sdacademy.java16poz.obiekty;
+
+/**
+ * User
+ *
+ * @author: Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @date: 24.03.2019 15:40
+ **/
+public class User {
+}
