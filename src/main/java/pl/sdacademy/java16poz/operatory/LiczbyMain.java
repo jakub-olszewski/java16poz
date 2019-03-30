1681e66a2a425af79700b58e38a5854f3e0f977a
Jakub Olszewski <jakub@olszewski.pl>
2019-03-30 12:30:21 +0100

    Zadanie: Klasa Liczba

--- src/main/java/pl/sdacademy/java16poz/operatory/LiczbyMain.java
+++ src/main/java/pl/sdacademy/java16poz/operatory/LiczbyMain.java
@@ -10,7 +10,10 @@ public class LiczbyMain {
 
     /**
      * Zadanie stwórz klasę liczba
+     * posiada konstruktor z argumentem double
      * posiada konstruktor z argumentem int
+     * posiada konstruktor z argumentem float
+     * pole wartosc jest double
      * posiada metody:
      * * jest parzysta
      * * jest nieparzysta
@@ -19,5 +22,20 @@ public class LiczbyMain {
      * * doPotegi(int potega) podnosi i zwraca potege
      * * dodaj(Liczba a) dodaje i zwraca liczbę
      * * odejmij(Liczba a) odejmuje i zwraca liczbę
+     * * wartosc() zwraca wartość int
+     * * toString() zwraca liczbe jako napis
      */
+
+//    Liczba a = new Liczba(3);
+//    Liczba b = new Liczba(5);
+//    Liczba c = a.dodaj(b); // 8
+//
+//    Liczba d = new Liczba(3.14f);
+//    System.out.println(d);
+//    double wartoscD = d.wartosc();
+//
+//    // podniesienie do kwadratu
+//    Liczba bDoKwadratu = b.doKwadratu();// 25
+//    // wykonanie i przetestowanie wszystkich metod
+//
 }
