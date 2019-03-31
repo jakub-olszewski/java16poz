2e9f61dbb044508428e63e8aa2ad483ccb217a9a
Jakub Olszewski <jakub@olszewski.pl>
2019-03-31 13:27:57 +0200

    Teoria: Tablice

---
+++ src/main/java/pl/sdacademy/java16poz/tablice/TabliceMain.java
@@ -0,0 +1,17 @@
+package pl.sdacademy.java16poz.tablice;
+
+/**
+ * TabliceMain
+ *
+ * @author: Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @date: 31.03.2019 13:27
+ **/
+public class TabliceMain {
+
+    public static void main(String[] args) {
+        /**
+         * Utwórz tablice zawierającą 4 liczby
+         * Utwórz tablice zawierającą 5 Miast
+         */
+    }
+}
