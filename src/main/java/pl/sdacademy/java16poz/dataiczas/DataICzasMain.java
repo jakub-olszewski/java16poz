46db264309a65dd06417490305fa6011d0fc9485
Jakub Olszewski <jakub@olszewski.pl>
2019-03-31 10:59:51 +0200

    Zadanie: Data i czas formatowanie

--- src/main/java/pl/sdacademy/java16poz/dataiczas/DataICzasMain.java
+++ src/main/java/pl/sdacademy/java16poz/dataiczas/DataICzasMain.java
@@ -13,26 +13,36 @@ public class DataICzasMain {
 
     public static void main(String[] args) {
         {
+            System.out.println("=== zamienianie daty na napis === ");
             // zamienianie daty na napis
             LocalDateTime obecnaData = LocalDateTime.now();
             String formatDaty = "yyyy-MM-dd HH:mm:ss";
+
             // formaterDaty - formatuje date według formatu 'formatDaty'
             DateTimeFormatter formaterDaty = DateTimeFormatter.ofPattern(formatDaty);
             // metoda format zmienia datę na napis
             String dataSformatowanaJakoNapis = obecnaData.format(formaterDaty);
-
-            System.out.println("Data sformatowana: " + dataSformatowanaJakoNapis);
+            
             System.out.println("Format daty: " + formatDaty);
             System.out.println("Data obiekt: " + obecnaData);
+            System.out.println("Data jako napis: " + dataSformatowanaJakoNapis);
+
+            System.out.println("---------------------------------------- ");
+
         }
         {
+            System.out.println("=== zamienia napis na date === ");
             // zamienia napis na date
             String dataString = "2016-03-04 11:30";
+            System.out.println("Napis: "+dataString);
             String formatDaty = "yyyy-MM-dd HH:mm";
+            System.out.println("Format: "+formatDaty);
 
             DateTimeFormatter formaterDaty = DateTimeFormatter.ofPattern(formatDaty);
             // parser - zmienianie napisu na date typu LocalDateTime
             LocalDateTime dataObiekt = LocalDateTime.parse(dataString, formaterDaty);
+            System.out.println("DataObiekt: "+dataObiekt);
+            System.out.println("---------------------------------------- ");
         }
         LocalDateTime data1 = utworzDateZNapisu("yyyy-MM-dd","2019-02-02");
         LocalDateTime data2 = utworzDateZNapisu("yyyy-MM-dd","2019-01-22");
