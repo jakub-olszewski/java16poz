e2b3271d4360ae519e08d0443e19b925efcab57b
Jakub Olszewski <jakub@olszewski.pl>
2019-03-31 12:28:51 +0200

    Zadanie: Switch

---
+++ src/main/java/pl/sdacademy/java16poz/instrukcje/przelacznik/StatusSwitchMain.java
@@ -0,0 +1,17 @@
+package pl.sdacademy.java16poz.instrukcje.przelacznik;
+
+/**
+ * StatusSwitchMain
+ *
+ * @author: Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @date: 31.03.2019 12:27
+ **/
+public class StatusSwitchMain {
+
+    /**
+     * Zadanie:
+     * Wykorzystując typ wyliczeniowy – enum  Status ( uzytkownika)
+     * i switch utwórz metodę wypisująca komunikat w zależności od statusu.
+     * DOSTEPNY,NIEDOSTEPNY,NIEAKTYWNY
+     */
+}
