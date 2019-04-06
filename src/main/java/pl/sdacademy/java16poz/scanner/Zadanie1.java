5a302f1bc127292b917c8a4ebdecd6e898ac2cb7
Jakub Olszewski <jakub@olszewski.pl>
2019-04-06 13:15:47 +0200

    Zadanie: Scanner

---
+++ src/main/java/pl/sdacademy/java16poz/scanner/Zadanie1.java
@@ -0,0 +1,20 @@
+package pl.sdacademy.java16poz.scanner;
+
+/**
+ * Zadanie1
+ *
+ * @author: Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @date: 06.04.2019 13:11
+ **/
+public class Zadanie1 {
+
+    public static void main(String[] args) {
+        /**
+         * Utwórz tablice zawierającą liczby wczytane za pomocą scanner
+         * wypisz największą liczbę
+         *
+         * Dla chętnych:
+         * Utwórz tablice zawierającą liczby wylosowane z użyciem Random()
+         */
+    }
+}
