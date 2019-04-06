c249de8493be61895dfc1fa1ece1e31841e8e794
Jakub Olszewski <jakub@olszewski.pl>
2019-04-06 10:10:23 +0200

    Zadanie: Petle for, while

---
+++ src/main/java/pl/sdacademy/java16poz/petle/Zadanie1.java
@@ -0,0 +1,23 @@
+package pl.sdacademy.java16poz.petle;
+
+/**
+ * Zadanie2
+ *
+ * @author: Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @date: 06.04.2019 10:05
+ **/
+public class Zadanie1 {
+    public static void main(String[] args) {
+        /**
+         * Zadanie1
+         * Wypisz elementy utworzonej wcześniej Listy zawierającą imiona na literę ‚a’
+         * lista 10 imion
+         *
+         * wersja:
+         * a) for
+         * b) while
+         *
+         */
+
+    }
+}
---
+++ src/main/java/pl/sdacademy/java16poz/petle/Zadanie2.java
@@ -0,0 +1,25 @@
+package pl.sdacademy.java16poz.petle;
+
+/**
+ * Zadanie1
+ *
+ * @author: Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @date: 06.04.2019 10:05
+ **/
+public class Zadanie2 {
+    public static void main(String[] args) {
+        /**
+         * Wypisz parzyste elementy utworzonej wcześniej tablicy zawierającą liczby
+         * tablica 10 liczb od 0 do 1000
+         * + dodatkowo użycie Random() czyli losowania
+         *
+         * 1. Stwórz tablice
+         * 2. Przypisz elementy
+         * 3. Wypisz
+         *
+         * wersja:
+         * a) for
+         * b) while
+         */
+    }
+}
