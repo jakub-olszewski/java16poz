9264477df2ee543ce57541880793632fcdc88538
Jakub Olszewski <jakub@olszewski.pl>
2019-04-06 14:40:23 +0200

    Zadanie: Abstrakcja i dziedziczenie

---
+++ src/main/java/pl/sdacademy/java16poz/abstrakcja/AbstrakcjaMain.java
@@ -0,0 +1,25 @@
+package pl.sdacademy.java16poz.abstrakcja;
+
+/**
+ * AbstrakcjaMain
+ *
+ * @author: Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @date: 06.04.2019 14:19
+ **/
+public class AbstrakcjaMain {
+
+    public static void main(String[] args) {
+
+        Figura figuraKwadrat = new Kwardrat(5);
+        figuraKwadrat.wypiszDane();
+
+        //TODO utwórz pozostałe figury i wypisz dane o nich
+        //wykorzystując klasę abstrakcyjną
+        //dodaj pola i obwody wszystkich figur
+        //wykorzystaj listy i pętle
+        //
+        //dodatkowo: wykorzystaj varargs
+        //           i oblicz sume obwodów
+
+    }
+}
