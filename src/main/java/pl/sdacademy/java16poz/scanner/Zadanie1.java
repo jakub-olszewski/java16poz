9552d92de09b223cbf49759ee2dde36c59a988a0
Jakub Olszewski <jakub@olszewski.pl>
2019-04-06 13:44:17 +0200

    Rozwiazanie: Wprowadzanie liczb do listy

--- src/main/java/pl/sdacademy/java16poz/scanner/Zadanie1.java
+++ src/main/java/pl/sdacademy/java16poz/scanner/Zadanie1.java
@@ -1,5 +1,10 @@
 package pl.sdacademy.java16poz.scanner;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * Zadanie1
  *
@@ -16,5 +21,42 @@ public class Zadanie1 {
          * Dla chętnych:
          * Utwórz tablice zawierającą liczby wylosowane z użyciem Random()
          */
+        List<Integer> liczbyWczytane = new ArrayList<>();
+        wczytajLiczby(liczbyWczytane);// z klawiatury
+        wypiszLiczby(liczbyWczytane);
+        wypiszNajwieksza(liczbyWczytane);// wypisz na konsoli
+
+    }
+
+    private static void wypiszLiczby(List<Integer> liczbyWczytane) {
+        System.out.println("Liczby:");
+        for(int liczba : liczbyWczytane){
+            System.out.print(liczba+" ");
+        }
+    }
+
+    private static void wypiszNajwieksza(List<Integer> liczbyWczytane) {
+
+        //w petli for porowynywac i zapamietywac obecna
+        //najwieksza
+
+    }
+
+    private static void wczytajLiczby(List<Integer> liczbyWczytane) {
+        //w petli ze skanerem wczytac
+        //ujemna kończy wczytywanie
+        System.out.println("Wprowadź liczby:");
+        Scanner skaner = new Scanner(System.in);
+        int wczytanaLiczba;
+        wczytanaLiczba = skaner.nextInt();
+        if(wczytanaLiczba>0){
+            liczbyWczytane.add(wczytanaLiczba);
+        }
+        while (wczytanaLiczba>0) {
+            wczytanaLiczba = skaner.nextInt();
+            if (wczytanaLiczba > 0) {
+                liczbyWczytane.add(wczytanaLiczba);
+            }
+        }
     }
 }
