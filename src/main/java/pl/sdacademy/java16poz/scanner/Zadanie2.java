7cc4fb840332d33e4490194892a845ce52686066
Jakub Olszewski <jakub@olszewski.pl>
2019-04-06 13:19:26 +0200

    Zadanie dodatkowe

---
+++ src/main/java/pl/sdacademy/java16poz/scanner/Zadanie2.java
@@ -0,0 +1,21 @@
+package pl.sdacademy.java16poz.scanner;
+
+/**
+ * Zadanie2
+ *
+ * @author: Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @date: 06.04.2019 13:13
+ **/
+public class Zadanie2 {
+
+    public static void main(String[] args) {
+        /**
+         * TODO Zadanie dodatkowe
+         * Pobierz za pomocą skanera tekst i podczas wypisytania
+         * zamieniaj brzydkie słowa na gwiazdki
+         *
+         * dodatkowo:
+         * ilość gwiazdek tyle ile liter
+         */
+    }
+}
