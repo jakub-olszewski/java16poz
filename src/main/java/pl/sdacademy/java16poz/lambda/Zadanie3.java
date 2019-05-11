0fa9fa59914b87a864d670a224611fbbdb89cbf7
Jakub Olszewski <jakub@olszewski.pl>
2019-05-11 15:52:01 +0200

    Zadanie domowe: Lambda osoba

---
+++ src/main/java/pl/sdacademy/java16poz/lambda/Zadanie3.java
@@ -0,0 +1,23 @@
+package pl.sdacademy.java16poz.lambda;
+
+import java.util.Arrays;
+
+/**
+ * Zadanie3
+ *
+ * @author: Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @date: 11.05.2019 15:42
+ **/
+public class Zadanie3 {
+
+    public static void main(String[] args) {
+
+        /**
+         * Zadanie : utwórz listę 10 obiektów Osoba. ( imie, nazwisko, wiek )
+         * Wyszukaj i wypisz w z użyciem stream() w jednej linii :
+         * - osoby o nazwisku Kowalski lub Kowalska
+         * - pełnoletnie
+         */
+
+    }
+}
