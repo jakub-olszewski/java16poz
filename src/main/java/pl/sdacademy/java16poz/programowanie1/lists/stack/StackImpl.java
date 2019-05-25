2149364311c99b40f07404c85ed520630e0b5543
Jakub Olszewski <jakub@olszewski.pl>
2019-05-25 10:37:55 +0200

    Zadanie: Testy stosu oraz implementacja - StackImpl

---
+++ src/main/java/pl/sdacademy/java16poz/programowanie1/lists/stack/StackImpl.java
@@ -0,0 +1,37 @@
+package pl.sdacademy.java16poz.programowanie1.lists.stack;
+
+import java.util.Optional;
+
+/**
+ * StackImpl - implementacja stosu
+ *
+ * @author: Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @date: 25.05.2019 10:35
+ **/
+public class StackImpl<T> implements Stack<T> {
+
+    @Override
+    public void push(T element) {
+
+    }
+
+    @Override
+    public Optional<T> pop() {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<T> peek() {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public String toCoolString() {
+        return null;
+    }
+}
