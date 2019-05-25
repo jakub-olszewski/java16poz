0b50863b75667b41860032e157bec6448e127dc0
Jakub Olszewski <jakub@olszewski.pl>
2019-05-25 13:07:08 +0200

    Rozwiązanie: Testy stosu oraz implementacja - StackImpl

--- src/main/java/pl/sdacademy/java16poz/programowanie1/lists/stack/StackImpl.java
+++ src/main/java/pl/sdacademy/java16poz/programowanie1/lists/stack/StackImpl.java
@@ -1,37 +1,69 @@
 package pl.sdacademy.java16poz.programowanie1.lists.stack;
 
+import pl.sdacademy.java16poz.programowanie1.lists.Node;
+
 import java.util.Optional;
 
 /**
- * StackImpl - implementacja stosu
+ * StackImpl
  *
  * @author: Jakub Olszewski [http://github.com/jakub-olszewski]
- * @date: 25.05.2019 10:35
+ * @date: 19.05.2019 01:22
  **/
 public class StackImpl<T> implements Stack<T> {
 
+    private Node<T> top;
+
     @Override
     public void push(T element) {
-
+        Node node = new Node<>(element);// tworzymy nowy node
+        if (isEmpty()) {//jeśli top jest nullem
+            top = node;// tworzymy nowy node
+        } else {// jesli top nie jest nullem
+            node.next = top;//jego następnikiem jest top
+            top = node;//nowy stworzony jest teraz top'em
+        }
     }
 
     @Override
     public Optional<T> pop() {
-        return Optional.empty();
+        if (isEmpty()) {//jeśli jest pusto
+            return Optional.empty();// zwróć pusty optional
+        } else {
+            Optional<T> num = Optional.of(top.value);// zapisanie tymczasowo wartości top
+            top = top.next;// teraz top'em będzie jego następnik
+            return num;
+        }
     }
 
     @Override
     public Optional<T> peek() {
-        return Optional.empty();
+        if (isEmpty()) {//jeśli stos jest pusty
+            return Optional.empty();//zwraca nam pusty optional
+        } else {
+            return Optional.of(top.value);//tworzenie i zwrócenie
+            // optionala z wartości top
+        }
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return top == null;
     }
+
+
 
     @Override
     public String toCoolString() {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        Node walker = top;
+
+        while (walker != null) {
+            sb.append("+-----+\n| ").append(walker.value).append(" |\n+-----+\n   |   \n");
+            walker = walker.next;
+        }
+        return sb.toString();
     }
+
+
 }
---
+++ src/main/java/pl/sdacademy/java16poz/programowanie1/lists/stack/StackMain.java
@@ -0,0 +1,28 @@
+package pl.sdacademy.java16poz.programowanie1.lists.stack;
+
+import pl.sdacademy.java16poz.obiekty.Book;
+
+/**
+ * StackMain
+ *
+ * @author: Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @date: 25.05.2019 13:01
+ **/
+public class StackMain {
+
+    public static void main(String[] args) {
+        Stack stack = new StackImpl();
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+
+        System.out.println(stack.toCoolString());
+        stack.pop();
+        System.out.println(stack.toCoolString());
+        stack.pop();
+        System.out.println(stack.toCoolString());
+        stack.pop();
+    }
+}
